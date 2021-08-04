c493609e97eded00acaa013b2fe4e02db372b697
Sviatlana Hodzina <72805375+SvetlanaHodzina@users.noreply.github.com>
2021-08-04 10:04:47 +0300

    Update WebDriverConnector.java

--- src/test/java/com/epam/autotesting/driver/WebDriverConnector.java
+++ src/test/java/com/epam/autotesting/driver/WebDriverConnector.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverConnector{
+    
     private static WebDriver driver;
 
     public static synchronized WebDriver getDriver(){
@@ -15,17 +16,14 @@ public class WebDriverConnector{
                 case "firefox": {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
-                    System.setProperty("webdriver.firefox.driver", "C:\\Webdrivers\\geckodriver.exe");
                 }
                 case "msedge": {
                     WebDriverManager.edgedriver().setup();
                     driver = new EdgeDriver();
-                    System.setProperty("webdriver.msedge.driver", "C:\\Webdrivers\\msedgedriver.exe");
                 }
                 default: {
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
-                    System.setProperty("webdriver.chrome.driver", "C:\\Webdrivers\\chromedriver.exe");
                 }
             }
             driver.manage().window().maximize();
