20fdd0379bfee1108ecaf957c0e6d226d12e14b3
Sviatlana Hodzina <72805375+SvetlanaHodzina@users.noreply.github.com>
2021-11-08 09:11:35 +0300

    Updated SearchResultsCloudGooglePage 2021/08/11
    Updation of  SearchResultsCloudGooglePage is due to changeable WebElement for  pricingCalculatorLink

--- src/test/java/com/epam/autotesting/page/SearchResultsCloudGooglePage.java
+++ src/test/java/com/epam/autotesting/page/SearchResultsCloudGooglePage.java
@@ -2,6 +2,7 @@ package com.epam.autotesting.page;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -12,11 +13,15 @@ import java.time.Duration;
 public class SearchResultsCloudGooglePage extends AbstractPage {
 
     private final Logger logger = LogManager.getRootLogger();
+
     @FindBy(xpath = "//h1[@class='devsite-search-title']")
     public static WebElement searchResults;
 
-    @FindBy(xpath = "//div[@class='gsc-thumbnail-inside']//b[text()='Google Cloud Platform Pricing Calculator']")
+    @FindBy(xpath = "//b[text()='Google Cloud Platform Pricing Calculator']")
     WebElement pricingCalculatorLink;
+
+    @FindBy(xpath = "//a[@href='https://cloud.google.com/products/calculator?hl=en']")
+    WebElement pricingCalculatorHrefLink;
 
     @Override
     public SearchResultsCloudGooglePage openPage() {
@@ -28,7 +33,14 @@ public class SearchResultsCloudGooglePage extends AbstractPage {
 
     public PricingCalculatorPage linkToPricingCalculatorLink() {
         new WebDriverWait(driver, Duration.ofSeconds(this.WAIT_TIMEOUT_SECONDS))
-                .until(ExpectedConditions.elementToBeClickable(pricingCalculatorLink)).click();
+                .until(ExpectedConditions.elementToBeClickable(pricingCalculatorLink));
+        if (driver.findElement(By.xpath("//b[text()='Google Cloud Platform Pricing Calculator']")).isEnabled()) {
+            new WebDriverWait(driver, Duration.ofSeconds(this.WAIT_TIMEOUT_SECONDS))
+                    .until(ExpectedConditions.elementToBeClickable(pricingCalculatorLink)).click();
+        } else if (driver.findElement(By.xpath("//a[@href='https://cloud.google.com/products/calculator?hl=en']")).isEnabled()) {
+            new WebDriverWait(driver, Duration.ofSeconds(this.WAIT_TIMEOUT_SECONDS))
+                    .until(ExpectedConditions.elementToBeClickable(pricingCalculatorHrefLink)).click();
+        }
         logger.info("You are linked to 'Pricing Calculator Page'");
         return new PricingCalculatorPage();
     }
